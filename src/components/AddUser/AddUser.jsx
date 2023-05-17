fc38354e09f3d8f3957892575d20979c7440b920
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-05-17 11:25:51 +0600

    send a new user to the server and loaded all users to display in the home page

--- src/components/AddUser/AddUser.jsx
+++ src/components/AddUser/AddUser.jsx
@@ -1,6 +1,41 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const AddUser = () => {
+    const [gender, setGender] = useState(null)
+    const [status, setStatus] = useState(null)
+
+    const handleAddUser = (event) => {
+        event.preventDefault()
+
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+
+        const user = { name, email, gender, status }
+
+        fetch('http://localhost:5000/add-user', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.insertedId) {
+                    Swal.fire(
+                        'Added!',
+                        'Successfully added a new user.',
+                        'success'
+                    )
+                    form.reset()
+                }
+            })
+    }
+
     return (
         <div className='my-5 w-75 mx-auto'>
             <Link to='/'><p className='px-3 py-2 text-info fw-semibold rounded border shadow-sm d-inline-block'>All Users</p></Link>
@@ -8,7 +43,7 @@ const AddUser = () => {
                 <h3>New User</h3>
                 <p>Use the below form to create a new user</p>
             </div>
-            <form className="w-75 mx-auto">
+            <form onSubmit={handleAddUser} className="w-75 mx-auto">
                 <div className="mb-4">
                     <label htmlFor="name" className="w-100 mb-2 fw-semibold">Name</label>
                     <input type="text" name="name" id="name" className="w-100 border p-2 rounded" placeholder="You name" required />
@@ -19,21 +54,21 @@ const AddUser = () => {
                 </div>
                 <div className="mb-4">
                     <span className="fw-semibold me-3">Gender</span>
-                    <input type="radio" name="gender" id="male" className="ms-4 me-1"/>
+                    <input type="radio" name="gender" id="male" className="ms-4 me-1" onChange={() => setGender("male")} />
                     <label htmlFor="male" className="mb-2 fw-semibold">Male</label>
 
-                    <input type="radio" name="gender" id="female" className="ms-4 me-1"/>
+                    <input type="radio" name="gender" id="female" className="ms-4 me-1" onChange={() => setGender("female")} />
                     <label htmlFor="female" className="mb-2 fw-semibold">Female</label>
                 </div>
                 <div className="mb-4">
                     <span className="fw-semibold me-3">Status</span>
-                    <input type="radio" name="status" id="active" className="ms-4 me-1"/>
+                    <input type="radio" name="status" id="active" className="ms-4 me-1" onChange={() => setStatus("active")} />
                     <label htmlFor="active" className="mb-2 fw-semibold">Active</label>
 
-                    <input type="radio" name="status" id="inactive" className="ms-4 me-1"/>
+                    <input type="radio" name="status" id="inactive" className="ms-4 me-1" onChange={() => setStatus("inactive")} />
                     <label htmlFor="inactive" className="mb-2 fw-semibold">Inactive</label>
                 </div>
-                <input type="submit" value="Save" className="bg-info w-100 py-2 px-3 border-0 fw-semibold rounded"/>
+                <input type="submit" value="Add User" className="bg-info w-100 py-2 px-3 border-0 fw-semibold rounded" />
             </form>
         </div>
     );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/all-users')
+        .then(res => res.json())
+        .then(data => {
+            setUsers(data)
+        })
+    }, [])
+
     return (
         <div className='my-5 w-75 mx-auto'>
         <Link to='/add-user'><p className='px-3 py-2 text-info fw-semibold rounded border shadow-sm d-inline-block'>New User</p></Link>
@@ -16,17 +27,21 @@ const Home = () => {
             </tr>
           </thead>
           <tbody>
-              <tr className='border-bottom'>
-                <td>1</td>
-                <td>Rana Sheikh</td>
-                <td>ranasheikhsbd@gmail.com</td>
-                <td>Male</td>
-                <td>Active</td>
+              {
+                users.map((user, index) => <tr className='border-bottom'
+                key={user._id}
+                >
+                <td>{index + 1}</td>
+                <td>{user?.name}</td>
+                <td>{user?.email}</td>
+                <td>{user?.gender}</td>
+                <td>{user?.status}</td>
                 <td>
                     <button className="me-2 py-2 px-3 rounded shadow-sm text-info bg-white border fw-semibold">Edit</button>
                     <button className=" py-2 px-3 rounded shadow-sm text-info bg-white border fw-semibold">Delete</button>
                 </td>
-              </tr>
+              </tr>)
+              }
           </tbody>
         </table>
         </div>
